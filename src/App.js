7e42387f3ce344533514dcd0e1d70882fb31a340
myelin0 <emi.veisllari@gmail.com>
2022-06-20 11:52:20 +0200

    Crated navbar with underline active navigation links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header';
+import Rockets from './components/Rockets';
+import MyProfile from './components/MyProfile';
+import Missions from './components/Missions';
 
+const App = () => (
+  <main className="App">
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+      </Routes>
+    </BrowserRouter>
+  </main>
+);
 export default App;
