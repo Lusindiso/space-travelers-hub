ea243c9b18b39ecbfecc660181255705b1d0a682
lUCI <lusindisontanjana@gmail.com>
2022-06-20 13:03:06 +0200

    connect store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-// import { Provider } from 'react-redux';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import store from "./redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>
 );
