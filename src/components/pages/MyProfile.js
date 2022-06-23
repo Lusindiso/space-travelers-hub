bf55817cb03477c07070f24c6ba7577d85b61266
myelin0 <emi.veisllari@gmail.com>
2022-06-23 16:16:24 +0200

    Display joined missions in My profile

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function MyProfile() {
+const Profile = () => {
+  const joinMissions = useSelector((state) => state.mission);
+  // const reserveRockets = useSelector((state) => state.post);
+  const filteredMissions = joinMissions.filter(
+    (mission) => mission.canceled === true,
+  );
+
   return (
-    <div>
-      profile under construction
+    <div className="container d-flex justify-content-center my-5">
+      <div>
+        <h2>My Missions</h2>
+        <div className="card mission-card">
+          <ul className="list-group list-group-flush">
+            {filteredMissions.length ? (
+              filteredMissions.map((mission) => {
+                const { id, name } = mission;
+                return (
+                  <li key={id} className="list-group-item">
+                    {name}
+                  </li>
+                );
+              })
+            ) : (
+              <p className="p-2 text-danger">Please join a mission</p>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default MyProfile;
+export default Profile;
