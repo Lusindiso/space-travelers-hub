62ba7391cbb89cdc0c829296c3feb466be493bbf
myelin0 <emi.veisllari@gmail.com>
2022-06-24 14:27:26 +0200

    Fix eslint error

--- src/components/__TEST__/Rockets.test.js
+++ src/components/__TEST__/Rockets.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../redux/index';
-import Rockets from '../pages/rockets';
+import Rockets from '../pages/Rockets';
 
 describe('Testing rocket component', () => {});
 it('should render correctly Rockets component', () => {
